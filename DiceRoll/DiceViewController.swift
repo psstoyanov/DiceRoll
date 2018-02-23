d85cd12957f563d61503ca422b4654e00e1af767
PakoSt <kokotekilata@gmail.com>
2018-02-23 14:04:55 +0000

    L2_E9: Replicate the Example UI
    Re-create the UI from the example Udacity project Dice.

---
+++ DiceRoll/DiceViewController.swift
@@ -0,0 +1,24 @@
+//
+//  DiceViewController.swift
+//  DiceRoll
+//
+//  Created by PakoSt on 2/23/18.
+//  Copyright © 2018 PakoSt. All rights reserved.
+//
+
+import UIKit
+
+class DiceViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
