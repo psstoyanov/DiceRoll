290cdb83f190c01e0c55bf9aead7f40b46933709
PakoSt <kokotekilata@gmail.com>
2018-02-23 14:36:44 +0000

    L2_E9: Connect IBActions and Outlets

--- DiceRoll/DiceViewController.swift
+++ DiceRoll/DiceViewController.swift
@@ -6,9 +6,22 @@
 //  Copyright © 2018 PakoSt. All rights reserved.
 //
 
+import Foundation
 import UIKit
 
 class DiceViewController: UIViewController {
+    
+    // MARK: Properties
+    
+    var firstValue: Int?
+    var secondValue: Int?
+    
+    // MARK: Outlets
+    
+    @IBOutlet var firstDie: UIImageView!
+    @IBOutlet var secondDie: UIImageView!
+    
+    // MARK: Life Cycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,5 +33,44 @@ class DiceViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        
+        // The dice will only appear if firstValue and secondValue have been set
+        if let firstValue = self.firstValue {
+            self.firstDie.image = UIImage(named: "d\(firstValue)")
+        } else {
+            self.firstDie.image = nil;
+        }
+        
+        if let secondValue = self.secondValue {
+            self.secondDie.image = UIImage(named: "d\(secondValue)")
+        } else {
+            self.secondDie.image = nil
+        }
+        
+        self.firstDie.alpha = 0
+        self.secondDie.alpha = 0
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        UIView.animate(withDuration: 0.3) {
+            self.firstDie.alpha = 1
+            self.secondDie.alpha = 1
+        }
+    }
+    
+    /**
+     * accepts a conditional Int, and returns an dice image, or nil
+     */
+    func imageForValue(_ value: Int?) -> UIImage? {
+        return nil
+    }
+    
+    /**
+     *    dismiss this view controller
+     */
+    func dismiss() {
+        self.dismiss(animated: true, completion: nil)
+    }
     
 }
--- DiceRoll/RollViewController.swift
+++ DiceRoll/RollViewController.swift
@@ -19,6 +19,32 @@ class RollViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    // MARK: Generate Dice Value
+    
+    /**
+     * Randomly generates a Int from 1 to 6
+     */
+    func randomDiceValue() -> Int {
+        // Generate a random Int32 using arc4Random
+        let randomValue = 1 + arc4random() % 6
+        
+        // Return a more convenient Int, initialized with the random value
+        return Int(randomValue)
+    }
+    
+    // MARK: Actions
+    
+    @IBAction func rollTheDice() {
+        var controller: DiceViewController
+        
+        controller = self.storyboard?.instantiateViewController(withIdentifier: "DiceViewController") as! DiceViewController
+        
+        controller.firstValue = self.randomDiceValue()
+        controller.secondValue = self.randomDiceValue()
+        
+        present(controller, animated: true, completion: nil)
+    }
 
 
 }
